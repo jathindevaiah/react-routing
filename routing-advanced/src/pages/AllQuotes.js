ee91ff9f99a79e4188b7637cc9e4a69a8217f974
Jathin Devaiah <devaiah.jathin@gmail.com>
2021-06-09 21:39:13 -0400

    adding Layout and MainNavigation components

--- routing-advanced/src/pages/AllQuotes.js
+++ routing-advanced/src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'John', text: 'Learning react!' },
+  { id: 'q2', author: 'James', text: 'Awesome react!' },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
