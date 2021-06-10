a83cf439e5acf134355104625e9dff8d4aadbdd2
Jathin Devaiah <devaiah.jathin@gmail.com>
2021-06-09 21:47:09 -0400

    Output data on Details page

--- routing-advanced/src/pages/QuoteDetail.js
+++ routing-advanced/src/pages/QuoteDetail.js
@@ -1,14 +1,25 @@
 import { Fragment } from 'react';
 import { useParams, Route } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'John', text: 'Learning react!' },
+  { id: 'q2', author: 'James', text: 'Awesome react!' },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!!</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
