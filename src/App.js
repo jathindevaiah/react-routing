8eda551d975af7d8509c966697192b6db720208f
Jathin Devaiah <devaiah.jathin@gmail.com>
2021-06-09 20:03:56 -0400

    Adding Header with links, using Link components for redirections

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { Route } from 'react-router-dom';
+import MainHeader from './components/MainHeader';
 import Products from './components/Products';
 import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div>
-      {/* 2 ways of defining routes */}
-      <Route path='/welcome' component={Welcome} />
-      <Route path='/products'>
-        <Products />
-      </Route>
+      <header>
+        <MainHeader />
+      </header>
+      <main>
+        {/* 2 ways of defining routes */}
+        <Route path='/welcome' component={Welcome} />
+        <Route path='/products'>
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
